0084758d608d9f52bb254e97e6780bfc7d69fdad
valdirdev22 <107660813+valdirdev22@users.noreply.github.com>
2022-12-23 09:06:52 -0300

    rockt commit

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
+import { NativeBaseProvider } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Routes } from './src/routes';
+
+import { THEME } from './src/theme';
+import { Loading } from './src/components/Loading';
+
+import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId('dd6753a9-19ad-4f0f-b1c3-700cff7f9dcc')
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <CartContextProvider>
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </CartContextProvider>
+    </NativeBaseProvider>
+  );
+}
